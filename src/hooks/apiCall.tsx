3d15d2afda1b384685f82bdccb44ab8b53d18ec0
hwang <nmc2711@naver.com>
2022-03-29 14:19:42 +0900

    feat(query): react-query default setting

---
+++ src/hooks/apiCall.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function apiCall() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default apiCall
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { RouteType } from './types/routeType'
 
 import RouterView from './hooks/useRoute';
@@ -10,24 +12,39 @@ import routes from './routes';
 
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+      refetchOnMount: false, // 쿼리가 마운트 시 date를 다시 가져올지 유무
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchInterval: false,
+    },
+  }
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RouterView
-        routes={routes}
-        onEnter={function (to: string, next: (path?: string) => void): void {
-          function checkElementsInArr(element: RouteType) {
-            if (element.path === to) {
-              return true;
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <RouterView
+          routes={routes}
+          onEnter={function (to: string, next: (path?: string) => void): void {
+            function checkElementsInArr(element: RouteType) {
+              if (element.path === to) {
+                return true;
+              }
             }
-          }
-          if (!(routes.some(checkElementsInArr))) {
-            next('/404');
-          }
-        }}
-      >
-      </RouterView>
-    </BrowserRouter>
+            if (!(routes.some(checkElementsInArr))) {
+              next('/404');
+            }
+          }}
+        >
+        </RouterView>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
