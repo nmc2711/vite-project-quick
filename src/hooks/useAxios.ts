e18606b1f64fe2cc2da56ccdaa81c5997c6a5079
hwang <nmc2711@naver.com>
2022-03-30 22:02:17 +0900

    common axios setting

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,19 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
 
-export const useAxios = {
-  get: <T>(url: string, params?: object) => {
-    axios.get<T>(url, {
-      ...params,
-    })
-  },
-  post: <T>(url: string, data: any) => {
-    axios.post<T>(url, data, {
-      
-    })
-  },
-  delete: <T>(url: string) => {
-    axios.delete<T>(url, {
-      
-    })
-  }
-};
+export const useAxios = <T>(config: AxiosRequestConfig) =>
+  useQuery<AxiosResponse<T>, AxiosError<T>>([config.url, config.params].filter(Boolean), () =>
+  axios.request<T>(config)
+);
