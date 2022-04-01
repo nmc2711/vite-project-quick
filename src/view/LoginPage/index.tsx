70dfed3a78e14320111e17337e617d6738aceb57
hwangsanghan <nmc2711@naver.com>
2022-04-01 18:36:51 +0900

    mutation test

--- src/view/LoginPage/index.tsx
+++ src/view/LoginPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useQuery, useMutation } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
 
 import { TUsers } from '~/types/api/users';
 
@@ -7,9 +7,23 @@ import apiCall from '~/util/apiCall';
 
 function LoginPage() {
   const { get, post } = apiCall();
+  const queryClient = useQueryClient();
+
   const { data } = useQuery<TUsers[], Error>("users", () => get('users', { id: 1230 }));
 
-  const mutaion = useMutation((id: number) => post("users", { id }));
+  const addMutaion = useMutation((text: TUsers) => post("users", text), {
+    onMutate: async text => {
+      await queryClient.cancelQueries("users");
+
+      const previousValue = queryClient.getQueriesData("users");
+
+      queryClient.setQueryData('users', (old: any) => [...old, text]);
+
+      return previousValue;
+    },
+  })
+
+  console.log(data)
 
   return (
     <div>
@@ -17,7 +31,13 @@ function LoginPage() {
       {data?.map(item => {
         return <div key={item.id}>{item.id}</div>
       })}
-      <button onClick={() => mutaion.mutate(100)}>유저 생성</button>
+      <button onClick={() => addMutaion.mutate({
+        id: 9999,
+        name: 'sanghan',
+        email: 'nmc2711@naver.com',
+        gender: 'm',
+        status: 'on',
+      })}>유저 생성</button>
     </div>
   );
 }
