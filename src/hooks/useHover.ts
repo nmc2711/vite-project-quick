b033a591d58871b2d9fbeb05bc6a7f6134022bd9
hwang <nmc2711@naver.com>
2022-04-18 15:46:27 +0900

    ismount 공통 함수 추가

---
+++ src/hooks/useHover.ts
@@ -0,0 +1,21 @@
+/**
+ * 최초 완전한 마운트 체크
+ */
+
+import { useCallback, useEffect, useRef } from 'react'
+
+function useIsMounted() {
+  const isMounted = useRef(false)
+
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  return useCallback(() => isMounted.current, [])
+}
+
+export default useIsMounted
