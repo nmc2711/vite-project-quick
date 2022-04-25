0405965cd778a8d8200d016a740c3accea6f5b5d
hwangsanghan <nmc2711@naver.com>
2022-04-25 14:53:06 +0900

    getclassname function 추가

---
+++ src/hooks/useGetClassName.ts
@@ -0,0 +1,21 @@
+import { isObject, isString } from 'lodash';
+
+function useGetClassName(...args: unknown[]) {
+  const className: string[] = [];
+
+  args.forEach((item: any) => {
+    if (isString(item)) {
+      className.push(item);
+    } else if (isObject(item)) {
+      Object.keys(item).forEach((key) => {
+        if (item[key]) {
+          className.push(key);
+        }
+      });
+    }
+  });
+
+  return className.join(' ');
+}
+
+export default useGetClassName;
