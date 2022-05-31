b84ab531c59c58c42cb7cebfa7de1684f703acef
hwang <nmc2711@naver.com>
2022-05-31 21:21:47 +0900

    use arr added

--- src/hooks/useFalsy.ts
+++ src/hooks/useFalsy.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 export const cleanObject = (obj: object) => {
@@ -10,3 +12,22 @@ export const cleanObject = (obj: object) => {
   });
   return result;
 };
+
+export const useArray = <T>(initialArray: T[]) => {
+    const [value, setValue] = useState(initialArray);
+    return {
+      value,
+      setValue,
+      add: (item: T) => {
+        setValue([...value, item]);
+      },
+      clear: () => {
+        setValue([]);
+      },
+      removeIndex: (index: number) => {
+        const copy = [...value];
+        copy.splice(index, 1);
+        setValue(copy);
+      },
+    };
+  };
